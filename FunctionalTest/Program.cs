dc46daa6ba2cecc269bbaca80fed723d75da8075
Michele <michele.trizio@gmail.com>
2026-08-16 09:26:15 +0200

    Added functional test

---
+++ FunctionalTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Emulator;
+using System.Reflection;
+
+namespace FunctionalTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {         
+            string baseTestLocation = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string testLocation = Path.Combine(baseTestLocation, @"Files\6502_functional_test.bin");
+
+            MOS6502 emulator = MOS6502.Instance;
+            emulator.CPU.PC = 0x400;
+
+            emulator.Run(testLocation);
+            Console.ReadKey();        
+        }
+    }
+}
